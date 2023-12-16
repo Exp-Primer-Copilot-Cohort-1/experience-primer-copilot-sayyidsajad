98bcd1020e1bf3fd13b7e554a47bee4483eac521
Sayyid Sajad Ottakath <111043430+sayyidsajad@users.noreply.github.com>
2023-12-16 08:46:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember(){
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberText = document.getElementById("memberText");
+    var skillsText = document.getElementById("skillsText");
+
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberText.style.color = "#fff";
+    skillsText.style.color = "#000";
+}
